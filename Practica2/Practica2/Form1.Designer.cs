00f74703b05e2197145cd723e353c9a931562180
FredyPelaez <36242866+FredyPelaez@users.noreply.github.com>
2018-02-28 19:57:09 -0600

    Add files via upload

---
+++ Practica2/Practica2/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Practica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.txtBoxShow = new System.Windows.Forms.TextBox();
+            this.cmdAbrir = new System.Windows.Forms.Button();
+            this.txtBoxNombre = new System.Windows.Forms.TextBox();
+            this.txtBoxEdad = new System.Windows.Forms.TextBox();
+            this.txtBoxTelefono = new System.Windows.Forms.TextBox();
+            this.txtBoxEscuela = new System.Windows.Forms.TextBox();
+            this.txtBoxCuenta = new System.Windows.Forms.TextBox();
+            this.cmdCrearXML = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // txtBoxShow
+            // 
+            this.txtBoxShow.Location = new System.Drawing.Point(12, 24);
+            this.txtBoxShow.Multiline = true;
+            this.txtBoxShow.Name = "txtBoxShow";
+            this.txtBoxShow.Size = new System.Drawing.Size(100, 96);
+            this.txtBoxShow.TabIndex = 0;
+            // 
+            // cmdAbrir
+            // 
+            this.cmdAbrir.Location = new System.Drawing.Point(21, 172);
+            this.cmdAbrir.Name = "cmdAbrir";
+            this.cmdAbrir.Size = new System.Drawing.Size(75, 23);
+            this.cmdAbrir.TabIndex = 1;
+            this.cmdAbrir.Text = "Abrir";
+            this.cmdAbrir.UseVisualStyleBackColor = true;
+            this.cmdAbrir.Click += new System.EventHandler(this.cmdAbrir_Click);
+            // 
+            // txtBoxNombre
+            // 
+            this.txtBoxNombre.Location = new System.Drawing.Point(150, 24);
+            this.txtBoxNombre.Name = "txtBoxNombre";
+            this.txtBoxNombre.Size = new System.Drawing.Size(172, 20);
+            this.txtBoxNombre.TabIndex = 2;
+            // 
+            // txtBoxEdad
+            // 
+            this.txtBoxEdad.Location = new System.Drawing.Point(150, 50);
+            this.txtBoxEdad.Name = "txtBoxEdad";
+            this.txtBoxEdad.Size = new System.Drawing.Size(172, 20);
+            this.txtBoxEdad.TabIndex = 3;
+            // 
+            // txtBoxTelefono
+            // 
+            this.txtBoxTelefono.Location = new System.Drawing.Point(150, 76);
+            this.txtBoxTelefono.Name = "txtBoxTelefono";
+            this.txtBoxTelefono.Size = new System.Drawing.Size(172, 20);
+            this.txtBoxTelefono.TabIndex = 4;
+            // 
+            // txtBoxEscuela
+            // 
+            this.txtBoxEscuela.Location = new System.Drawing.Point(150, 102);
+            this.txtBoxEscuela.Name = "txtBoxEscuela";
+            this.txtBoxEscuela.Size = new System.Drawing.Size(172, 20);
+            this.txtBoxEscuela.TabIndex = 5;
+            // 
+            // txtBoxCuenta
+            // 
+            this.txtBoxCuenta.Location = new System.Drawing.Point(150, 128);
+            this.txtBoxCuenta.Name = "txtBoxCuenta";
+            this.txtBoxCuenta.Size = new System.Drawing.Size(172, 20);
+            this.txtBoxCuenta.TabIndex = 6;
+            // 
+            // cmdCrearXML
+            // 
+            this.cmdCrearXML.Location = new System.Drawing.Point(183, 172);
+            this.cmdCrearXML.Name = "cmdCrearXML";
+            this.cmdCrearXML.Size = new System.Drawing.Size(98, 23);
+            this.cmdCrearXML.TabIndex = 7;
+            this.cmdCrearXML.Text = "Guardar XML";
+            this.cmdCrearXML.UseVisualStyleBackColor = true;
+            this.cmdCrearXML.Click += new System.EventHandler(this.cmdCrearXML_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 261);
+            this.Controls.Add(this.cmdCrearXML);
+            this.Controls.Add(this.txtBoxCuenta);
+            this.Controls.Add(this.txtBoxEscuela);
+            this.Controls.Add(this.txtBoxTelefono);
+            this.Controls.Add(this.txtBoxEdad);
+            this.Controls.Add(this.txtBoxNombre);
+            this.Controls.Add(this.cmdAbrir);
+            this.Controls.Add(this.txtBoxShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.TextBox txtBoxShow;
+        private System.Windows.Forms.Button cmdAbrir;
+        private System.Windows.Forms.TextBox txtBoxNombre;
+        private System.Windows.Forms.TextBox txtBoxEdad;
+        private System.Windows.Forms.TextBox txtBoxTelefono;
+        private System.Windows.Forms.TextBox txtBoxEscuela;
+        private System.Windows.Forms.TextBox txtBoxCuenta;
+        private System.Windows.Forms.Button cmdCrearXML;
+    }
+}
+
---
+++ Practica2/Practica2/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace Practica2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void CrearXml(XmlDocument doc, string nombre, string edad, string telefono, string escuela, string cuenta)
+        {
+            XmlDeclaration documento = doc.CreateXmlDeclaration("1.0", "utf-8", "yes");
+            XmlElement element = doc.DocumentElement;
+            doc.InsertBefore(documento, element);
+
+
+            XmlNode node = doc.CreateElement("ALumno");
+            doc.AppendChild(node);
+            XmlNode usNode = doc.CreateElement("Registros");
+            XmlAttribute name = doc.CreateAttribute("Nombre");
+            name.Value = nombre;
+            usNode.Attributes.Append(name);
+            XmlAttribute age = doc.CreateAttribute("Edad");
+            age.Value = edad;
+            usNode.Attributes.Append(age);
+            XmlAttribute Telefono = doc.CreateAttribute("Telefono");
+            Telefono.Value = telefono;
+            usNode.Attributes.Append(Telefono);
+            XmlAttribute Escuela = doc.CreateAttribute("Escuela");
+            Escuela.Value = escuela;
+            usNode.Attributes.Append(Escuela);
+            XmlAttribute Cuenta = doc.CreateAttribute("Cuenta");
+            Cuenta.Value = cuenta;
+            usNode.Attributes.Append(Cuenta);
+            node.AppendChild(usNode);
+        }
+        private void cmdCrearXML_Click(object sender, EventArgs e)
+        {
+            saveFile.Filter = ".xml|*.xml";
+            saveFile.ShowDialog();
+            XmlDocument xml = new XmlDocument();
+
+            CrearXml(xml, txtBoxNombre.Text, txtBoxEdad.Text, txtBoxTelefono.Text, txtBoxEscuela.Text, txtBoxCuenta.Text);
+
+            txtBoxNombre.Clear();
+            txtBoxTelefono.Clear();
+            txtBoxEdad.Clear();
+            txtBoxEscuela.Clear();
+            txtBoxCuenta.Clear();
+
+            xml.Save(saveFile.FileName);
+        }
+
+        private void cmdAbrir_Click(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+            FileStream archivo = new
+                FileStream(openFile.FileName, FileMode.Open);
+
+            BinaryReader leer = new BinaryReader(archivo);
+
+            Int16 bm = 0;
+            bm = leer.ReadInt16();
+            if (bm == 0x4D42)
+                txtBoxShow.Text = "BM" + Environment.NewLine;
+
+            Int32 size = 0;
+            size = leer.ReadInt32();
+            txtBoxShow.Text += size.ToString() + Environment.NewLine;
+
+            Int32 width = 0;
+            archivo.Seek(18, SeekOrigin.Begin);
+            width = leer.ReadInt32();
+            txtBoxShow.Text += width.ToString() + Environment.NewLine;
+
+            Int32 heigth = 0;
+            heigth = leer.ReadInt32();
+            txtBoxShow.Text += heigth.ToString() + Environment.NewLine;
+
+            archivo.Seek(28, SeekOrigin.Begin);
+
+            Int16 pixels = 0;
+            pixels = leer.ReadInt16();
+            txtBoxShow.Text += pixels.ToString() + Environment.NewLine;
+
+            leer.Close();
+            archivo.Close();
+        }
+    }
+}
