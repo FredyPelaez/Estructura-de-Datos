eefad5a697fa0872e64a6c96e37990aa9383be77
FredyPelaez <36242866+FredyPelaez@users.noreply.github.com>
2018-06-05 19:07:17 -0500

    Add files via upload

---
+++ Actividad.ListasEnlazadas/Actividad.ListasEnlazadas/Agenda.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad.ListasEnlazadas
+{
+    class Agenda
+    {
+        public int tamaño = 4;
+        private Contacto[] agenda = new Contacto[5];
+        public int contador = 0;
+        private Contacto inicio;
+        private string cadena = "";
+
+        public void agregarInicio(Contacto nuevo)
+        {
+            if (inicio == null) inicio = nuevo;
+            else
+                agregar(nuevo, inicio);
+        }
+
+        public void eliminarPrimero()
+        {
+            inicio = inicio.Siguiente;
+        }
+
+        public void eliminarUltimo()
+        {
+            Contacto x = inicio;
+
+            while (x.Siguiente  != null)
+            {
+                if (x.Siguiente.Siguiente == null) x.Siguiente = null;
+                else
+                    x = x.Siguiente;
+            }
+        }
+
+        public string reporteInversoUno(Contacto x)
+        {
+            x = inicio;
+            cadena = "";
+            if (x.Siguiente == null)
+            {
+                cadena += x.ToString() + Environment.NewLine;
+                return cadena;
+            }
+            else
+                reporteInverso(x);
+            return cadena;
+        }
+        public void reporteInverso(Contacto x)
+        {
+            if (x.Siguiente != null)
+            {
+                reporteInverso(x.Siguiente);
+                cadena += x.ToString() + Environment.NewLine;
+            }
+            else
+            {
+                cadena += x.ToString() + Environment.NewLine;
+            }
+        }
+
+        public void invertir()
+        {
+            Contacto x = inicio;
+
+            if (x.Siguiente.Siguiente != null)
+            {
+                Invertir(x);
+                
+                x.Siguiente = null;
+            }
+            else
+            {
+                x.Siguiente.Siguiente = x;
+                inicio = x.Siguiente;
+                x.Siguiente = null;
+            }
+        }
+                                                                           
+        public void Invertir(Contacto x)
+        {
+            if (x.Siguiente.Siguiente != null)
+            {
+                Invertir(x.Siguiente);
+                x.Siguiente.Siguiente = x;
+            }
+            else
+            {
+                x.Siguiente.Siguiente = x;
+                inicio = x.Siguiente;
+            }
+        }
+
+        public void agregar(Contacto nuevo, Contacto x)
+        {
+            if (x.Siguiente == null) x.Siguiente = nuevo;
+            else
+                agregar(nuevo, x.Siguiente);
+        }
+
+        public Contacto buscar(string telefono)
+        {
+            Contacto x = inicio;
+
+            while (x != null)
+            {
+                if (x.Telefono == telefono) return x;
+                x = x.Siguiente;
+            }
+
+            return null;
+        }
+
+        //public void eliminar(string telefono)
+        //{
+        //    Contacto x = inicio;
+        //    while (x.Siguiente.Telefono != telefono)
+        //    {
+        //        if (x.Siguiente.Siguiente.Telefono != telefono) x.Siguiente = null;
+        //        else
+        //            x = x.Siguiente;
+        //    }
+        //}
+
+        public string listar()
+        {
+            Contacto x = inicio;
+            cadena = "";
+
+            while(x != null)
+            {
+                cadena += x.ToString() + Environment.NewLine;
+                x = x.Siguiente;
+            }
+
+            return cadena;
+        }
+    }
+}
---
+++ Actividad.ListasEnlazadas/Actividad.ListasEnlazadas/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad.ListasEnlazadas
+{
+    public partial class Form1 : Form
+    {
+
+        Agenda agenda = new Agenda();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void clear()
+        {
+            txtBoxNumero.Text = "";
+            txtBoxNombre.Text = "";
+            txtBoxCorreo.Text = "";
+            txtBoxEdad.Text = "";
+            txtBoxAP.Text = "";
+            txtBoxAM.Text = "";
+        }
+
+        private void cmdAgregar_Click(object sender, EventArgs e)
+        {
+            Contacto cX = new Contacto();
+
+            cX.Nombre = txtBoxNombre.Text;
+            cX.APaterno = txtBoxAP.Text;
+            cX.AMaterno = txtBoxAM.Text;
+            cX.Telefono = txtBoxNumero.Text;
+            cX.Edad = txtBoxEdad.Text;
+            cX.Email = txtBoxCorreo.Text;
+
+            agenda.agregarInicio(cX);
+            MessageBox.Show("Contacto Agregado Correctamente", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+           
+            clear();
+        }
+
+        private void cmdBuscar_Click(object sender, EventArgs e)
+        {
+            Contacto contacto = agenda.buscar(txtBoxNumero.Text);
+
+            if (agenda.buscar(txtBoxNumero.Text) == null)
+            {
+                MessageBox.Show("Contacto No Encontrado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                clear();
+            }
+            else
+            {
+                txtBoxNombre.Text = contacto.Nombre;
+                txtBoxAP.Text = contacto.APaterno;
+                txtBoxAM.Text = contacto.AMaterno;
+                txtBoxNumero.Text = contacto.Telefono;
+                txtBoxEdad.Text = contacto.Edad;
+                txtBoxCorreo.Text = contacto.Email;
+                MessageBox.Show("Contacto Encontrado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void cmdEliminar_Click(object sender, EventArgs e)
+        {
+            agenda.eliminar(txtBoxNumero.Text);
+            MessageBox.Show("Contacto Eliminado Correctamente", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            clear();
+        }
+
+        private void cmdListar_Click(object sender, EventArgs e)
+        {
+            txtBoxListar.Clear();
+            txtBoxListar.Text = agenda.listar();
+        }
+
+        private void cmdEliminarPrimero_Click(object sender, EventArgs e)
+        {
+            agenda.eliminarPrimero();
+        }
+
+        private void cmdEliminarUltimo_Click(object sender, EventArgs e)
+        {
+            agenda.eliminarUltimo();
+        }
+
+        private void cmdReporteInverso_Click(object sender, EventArgs e)
+        {
+            Contacto cX = new Contacto();
+            txtBoxListar.Clear();
+            txtBoxListar.Text = agenda.reporteInversoUno(cX);
+        }
+
+        private void cmdInvertir_Click(object sender, EventArgs e)
+        {
+            agenda.invertir();
+        }
+    }
+}
